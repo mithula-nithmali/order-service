7a23f41362b223fe440b89d3c6646e9cbf1cdfa4
mithula <mithula@dmsswt.com>
2026-02-07 18:26:07 +0530

    add circuit breaker

--- src/main/java/com/microservices/order_service/config/RestClientConfig.java
+++ src/main/java/com/microservices/order_service/config/RestClientConfig.java
@@ -1,27 +1,59 @@
 package com.microservices.order_service.config;
 
 import com.microservices.order_service.client.InventoryClient;
+import org.apache.hc.client5.http.config.RequestConfig;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.core5.util.Timeout;
 import org.springframework.beans.factory.annotation.Value;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+import java.time.Duration;
 
 @Configuration
 public class RestClientConfig {
 
     @Value("${inventory.url}")
     private String inventoryServiceUrl;
+
     @Bean
-    public InventoryClient inventoryClient(){
+    public InventoryClient inventoryClient() {
+
         RestClient restClient = RestClient.builder()
                 .baseUrl(inventoryServiceUrl)
+                .requestFactory(clientHttpRequestFactory())
                 .build();
 
-        var restClientAdapter = RestClientAdapter.create(restClient);
-        var httpServiceProxyFactory= HttpServiceProxyFactory.builderFor(restClientAdapter).build();
-        return httpServiceProxyFactory.createClient(InventoryClient.class);
+        RestClientAdapter adapter = RestClientAdapter.create(restClient);
+        HttpServiceProxyFactory factory =
+                HttpServiceProxyFactory.builderFor(adapter).build();
+
+        return factory.createClient(InventoryClient.class);
     }
+
+    @Bean
+    public ClientHttpRequestFactory clientHttpRequestFactory() {
+
+        RequestConfig requestConfig = RequestConfig.custom()
+                .setConnectTimeout(Timeout.ofSeconds(3))
+                .setResponseTimeout(Timeout.ofSeconds(3))
+                .build();
+
+        CloseableHttpClient httpClient = HttpClients.custom()
+                .setDefaultRequestConfig(requestConfig)
+                .build();
+
+        return new HttpComponentsClientHttpRequestFactory(httpClient);
+    }
+
+
 
 }
